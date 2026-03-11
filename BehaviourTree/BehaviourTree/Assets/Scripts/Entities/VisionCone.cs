b23fced1e3b77df9ece7a065483aca19af86ba49
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2026-03-11 16:12:33 +0100

    Update: vision cone

--- BehaviourTree/BehaviourTree/Assets/Scripts/Entities/VisionCone.cs
+++ BehaviourTree/BehaviourTree/Assets/Scripts/Entities/VisionCone.cs
@@ -23,23 +23,33 @@ public class VisionCone : MonoBehaviour
         if (!isGlobal) angleInDegrees -= transform.eulerAngles.z + offsetRotation.eulerAngles.z;
         return new Vector3(MathF.Sin(angleInDegrees * Mathf.Deg2Rad), MathF.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
     }
-    
+
     public GameObject[] GetCurrentlySeenObjects()
     {
         var hits = Physics2D.CircleCastAll(transform.position, viewDistance, Vector2.up, 0, targetLayerMask);
         if (hits.Length == 0)
             return Array.Empty<GameObject>();
-        
-        var threshold = -MathF.Cos(fieldOfView * Mathf.Deg2Rad);
+
         var visibleHits = hits.Where(raycastHit =>
         {
             var target = raycastHit.collider.transform;
             var dirToTarget = (target.position - transform.position).normalized;
 
-            if (!(Vector3.Dot(transform.forward, dirToTarget) < threshold)) return false;
+            var angle = Vector3.Angle(transform.right, dirToTarget);
+            if (!(angle < fieldOfView / 2))
+                return false;
             
             var distToTarget = Vector2.Distance(transform.position, target.transform.position);
-            return Physics2D.Raycast(transform.position, dirToTarget, distToTarget, obstacleLayerMask);
+            var result = Physics2D.Raycast(transform.position, dirToTarget, distToTarget + 1,
+                obstacleLayerMask + targetLayerMask);
+            if (!result) return false;
+            
+            if ((obstacleLayerMask.value & (1 << result.collider.gameObject.layer)) != 0)
+                return false;
+            if ((targetLayerMask.value & (1 << result.collider.gameObject.layer)) != 0)
+                return true;
+
+            return false;
         }).ToArray();
         
         var objects = visibleHits.Select(raycastHit => raycastHit.collider?.gameObject).ToArray();
@@ -49,14 +59,15 @@ public class VisionCone : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         var seen = GetCurrentlySeenObjects();
+        
         var color = Color.red;
-        color.a = 0.5f;
         Gizmos.color = color;
-        Debug.Log(seen.Length);
+        
+        // Debug.Log(seen.Length);
         foreach (var obj in seen)
         {
             if (!obj) continue;
-            Gizmos.DrawSphere(obj.transform.position, 0.5f);
+            Gizmos.DrawSphere(obj.transform.position + Vector3.back, 0.5f);
         }
     }
 }
